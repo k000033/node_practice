39b9e38492ba260972967934f1c3755463c9fbf5
k000033 <85823133+k000033@users.noreply.github.com>
2021-06-19 16:54:55 +0800

    first commit dev

--- node_practice/h01/b.js
+++ node_practice/h01/b.js
@@ -1,9 +1,9 @@
-const  {add,mul} = require('./a')
+const  {add,mul} = require('./a') // 將 a.js 的 add，mul 方法 引用
 const sum = add(10,20)
 const result = mul(10,10)
 
 
-const  _ = require('lodash')
+const  _ = require('lodash') // 引用 package.json 
 console.log(sum)
 console.log(result)
 
